9f6db02d7a12d7036318bd5b5c9e6e35a9f8cc57
danrkeith <77320308+danrkeith@users.noreply.github.com>
2021-07-28 10:02:43 +1000

    added SQLite.cs

---
+++ SQLite.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace ScoreCardv2
+{
+    public static class SQLite
+    {
+        public static SqliteCommand Command(SqliteConnection connection, string query, params (string key, object value)[] parameters)
+        {
+            SqliteCommand command = connection.CreateCommand();
+            command.CommandText = query;
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                command.Parameters.AddWithValue(parameters[i].key, parameters[i].value);
+            }
+            return command;
+        }
+    }
+}
