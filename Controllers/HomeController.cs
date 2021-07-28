fc27ebaaee58119076fb3134abfe26a0fe53de5c
danrkeith <77320308+danrkeith@users.noreply.github.com>
2021-07-29 09:50:07 +1000

    added user routes & user controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,27 +11,16 @@ namespace ScoreCardv2.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+        [Route("/")]
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
+            return View("/Views/Home/Index.cshtml");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View("/Views/Shared/Error.cshtml", new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScoreCardv2.Controllers
+{
+    public class UserController : Controller
+    {
+        [Route("/User/")]
+        public IActionResult Index()
+        {
+            return View("/Views/User/Index.cshtml");
+        }
+
+        [Route("/User/Login/")]
+        public IActionResult Login()
+        {
+            return View("/Views/User/Login.cshtml");
+        }
+
+        [Route("/User/Register/")]
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View("/Views/User/Register.cshtml");
+        }
+
+        [Route("/User/Register/")]
+        [HttpPost]
+        public IActionResult PostRegister()
+        {
+            return Redirect("/User/Register/");
+        }
+    }
+}
