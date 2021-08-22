e7f8db04f2653cdcf8b3881dcf721bb9b87cd8d0
danrkeith <77320308+danrkeith@users.noreply.github.com>
2021-08-22 19:08:44 +1000

    added basic structure for BlankGame

---
+++ Controllers/BlankGameController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScoreCardv2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScoreCardv2.Controllers
+{
+    public class BlankGameController : GameController<GameViewModel>
+    {
+        public BlankGameController() : base(
+            viewPath: "Views/BlankGame",
+            table: "blankGame",
+            controller: "BlankGame"
+            )
+        { }
+
+        [Route("/BlankGame/")]
+        [HttpGet]
+        public IActionResult Index() => BaseIndex();
+    }
+}
